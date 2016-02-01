1fb6f0472614c5648d63c18dfaede60ca896b7ee
venetucci <michelle.ann.harvey@gmail.com>
2016-01-31 17:17:41 -0800

    Added steps for creating account and signing in

---
+++ Dropboxish/CreateAccountViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CreateAccountViewController.swift
+//  Dropboxish
+//
+//  Created by Michelle Harvey on 1/26/16.
+//  Copyright © 2016 Michelle Venetucci Harvey. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Dropboxish/SignInRootViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignInRootViewController.swift
+//  Dropboxish
+//
+//  Created by Michelle Harvey on 1/26/16.
+//  Copyright © 2016 Michelle Venetucci Harvey. All rights reserved.
+//
+
+import UIKit
+
+class SignInRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        performSegueWithIdentifier("signInSegue", sender: self)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
